91d99684df6b37298ea7931916ae1ab566bb8f5a
David Křístek <david.kristek05@gmail.com>
2022-05-20 14:19:14 +0200

    without router

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,9 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
       <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
